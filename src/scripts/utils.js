5511905a42246adae2725c6e701655a487c58791
Jean-Marc Baubet <jean.marc.baubet@gmail.com>
2024-12-09 15:28:20 +0100

    Import des gpx a priori OK
    on passe cette version sur une branche maintenance

---
+++ src/scripts/utils.js
@@ -0,0 +1,11 @@
+/**
+ * Cette fonction retourne une chaine qui format le nombre n sur len caractères
+ *  exemple : zpad(10, 6) => 000010
+ *            zpad(12.87, 8) =>  00012.87
+ * @param {number} n 
+ * @param {number} len 
+ * @returns 
+ */
+export function zpad(n, len) {
+  return 0..toFixed(len).slice(2, -n.toString().length) + n.toString();
+}
